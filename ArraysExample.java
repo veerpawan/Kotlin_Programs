a5fd42de52c92628df827792d96bdebe9c61f76a
pawan soni <veerpawan888@gmail.com>
2020-08-22 15:08:39 +0530

    Add files via upload

---
+++ ArraysExample.java
@@ -0,0 +1,29 @@
+package DS;
+
+public class ArraysExample {
+
+
+    public static void main(String[] args){
+
+        int[] intArray=new int[7];
+
+
+        intArray[0]=10;
+        intArray[1]=20;
+        intArray[2]=10;
+        intArray[3]=10;
+        intArray[4]=10;
+        intArray[5]=10;
+        intArray[6]=10;
+
+        for(int i=0;i<intArray.length;i++){
+
+            System.out.println(intArray[i]);
+
+        }
+
+
+
+
+    }
+}
